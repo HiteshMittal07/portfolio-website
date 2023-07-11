102a7e0d3b38648a115dec94a432b1fba475def7
max11759 <smartyboy.hm15@gmail.com>
2023-07-12 01:03:52 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function runtypingEffect(){
+    const text='I am Hitesh Mittal';
+    const typingElement=document.getElementById('typing-text');
+    const delay=70;
+    typetext(text,typingElement,delay);
+
+}
+function typetext(text,typingElement,delay){
+    for(let i=0;i<text.length;i++){
+        setTimeout(()=>{
+            typingElement.textContent += text.charAt(i);
+        },delay *i)
+    }
+}
+document.addEventListener('DOMContentLoaded',runtypingEffect);
